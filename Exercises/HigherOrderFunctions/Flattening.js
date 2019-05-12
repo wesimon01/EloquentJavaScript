9a5168fb05c9700df4a40c648c637acf1d0fa0de
plutonium <wesimon01@gmail.com>
2019-05-11 22:55:13 -0500

    intial commit

---
+++ Exercises/HigherOrderFunctions/Flattening.js
@@ -0,0 +1,10 @@
+﻿//let arrays = [[1, 2, 3], [4, 5], [6]];
+// Your code here.
+// → [1, 2, 3, 4, 5, 6]
+
+
+function flatten(arrays) {
+    
+    console.log(arrays.reduce((flat, current) => flat.concat(current), []));
+
+};
